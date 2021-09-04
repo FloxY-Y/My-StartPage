3495e1b01f078e2740311f93e4f57de670876889
FloxY-Y <47297859+FloxY-Y@users.noreply.github.com>
2021-09-04 18:01:32 +0200

    Make button works

--- media/js/main.js
+++ media/js/main.js
@@ -19,3 +19,26 @@ function FUCK_GIT(key) {
         setTimeout(() => { location.href = "https://ohshitgit.com"; }, 500);
     }
 }
+
+let button_list = document.querySelectorAll(".opt");
+button_list.forEach(e => { e.addEventListener("click", () => serviceSelect(e.attributes["data-service"].value)); });
+
+let serviceSelected = "ddg";
+
+/**
+ * @param {MouseEvent} e 
+ */
+function serviceSelect(e) {
+    button_list.forEach(e => e.classList.remove("selected"));
+    if (e == serviceSelected) {
+        serviceSelected = "ddg";
+        return;
+    } else {
+        serviceSelected = e;
+        document.querySelector('[data-service="' + e + '"]').classList.add("selected");
+    }
+}
+
+document.forms[0].addEventListener("submit", () => {
+    document.forms[0][0].value = "!" + serviceSelected + " " + document.forms[0][0].value;
+});
